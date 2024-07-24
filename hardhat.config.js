8e2fc5fe3fab1495fa51ee7719a1414a797b0111
tinboy16 <thuongtin1602@gmail.com>
2024-07-24 23:53:17 +0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: ["0xdone"], 
+    },
+  }
+};
